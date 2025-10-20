c4ff37bc58bf85bdb35558a719e386ab55c6f69c
franciscovmn <fvmaianeto@gmail.com>
2025-10-20 11:24:09 -0300

    update: corrigido erro cascata apagar

--- src/util/Db4oUtil.java
+++ src/util/Db4oUtil.java
@@ -14,18 +14,21 @@ public class Db4oUtil {
     public static ObjectContainer abrirDB() {
         EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
 
-        // Cascata: atualizar e apagar listas associadas
+        // Configurar apenas cascadeOnUpdate
         ObjectClass pessoaClass = config.common().objectClass(Pessoa.class);
         pessoaClass.objectField("listaVacinacao").cascadeOnUpdate(true);
-        pessoaClass.objectField("listaVacinacao").cascadeOnDelete(true);
+        // GARANTIR QUE A LINHA ABAIXO FOI REMOVIDA OU COMENTADA:
+        // pessoaClass.objectField("listaVacinacao").cascadeOnDelete(true);
 
         ObjectClass vacinacaoClass = config.common().objectClass(Vacinacao.class);
-        vacinacaoClass.cascadeOnDelete(true);
         vacinacaoClass.cascadeOnUpdate(true);
+        // GARANTIR QUE A LINHA ABAIXO FOI REMOVIDA OU COMENTADA (se existia):
+        // vacinacaoClass.cascadeOnDelete(true);
 
         ObjectClass vacinaClass = config.common().objectClass(Vacina.class);
         vacinaClass.objectField("listaVacinacao").cascadeOnUpdate(true);
-        vacinaClass.objectField("listaVacinacao").cascadeOnDelete(true);
+        // GARANTIR QUE A LINHA ABAIXO FOI REMOVIDA OU COMENTADA:
+        // vacinaClass.objectField("listaVacinacao").cascadeOnDelete(true);
 
         return Db4oEmbedded.openFile(config, DATABASE_FILE);
     }
